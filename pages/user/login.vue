ab5962ce29a9a39e2ef08159ae2f04cb5106e929
caichenghua <xxx@xx.com>
2019-08-01 15:32:37 +0800

    01-头部底部完成

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,91 +1,13 @@
 <template>
-  <div class="container">
-    <!-- 主要内容 -->
-    <el-row type="flex" justify="center" align="middle" class="main">
-      <div class="form-wrapper">
-        <!-- 表单头部tab -->
-        <el-row type="flex" justify="center" class="tabs">
-          <span
-            :class="{active:curr==index}"
-            v-for="(val,index) in tabArr"
-            :key="index"
-            @click="changeTab(index)"
-          >{{val}}</span>
-        </el-row>
-        <!-- 登录功能组件 -->
-        <LoginForm v-if="curr===0" />
-        <!-- 注册功能组件 -->
-        <RegisterForm v-else />
-      </div>
-    </el-row>
-  </div>
+  <div class="login">登录</div>
 </template>
-<script>
-// 引入组件
-import RegisterForm from "@/components/user/registerForm";
-import LoginForm from "@/components/user/loginForm";
 
+<script>
 export default {
-    // 注册组件
-    components: {
-        RegisterForm,
-        LoginForm
-    },
-  data() {
-    return {
-      curr: 0,
-      tabArr: ["登录", "注册"]
-    };
-  },
-  methods: {
-    changeTab(index) {
-        this.curr=index
-    }
-  }
-};
+
+}
 </script>
 
-<style scoped lang="less">
-.container {
-  background: url(http://157.122.54.189:9095/assets/images/th03.jfif) center 0;
-  height: 700px;
-  min-width: 1000px;
+<style lang="less" scoped>
 
-  .main {
-    width: 1000px;
-    height: 100%;
-    margin: 0 auto;
-    position: relative;
-
-    .form-wrapper {
-      width: 400px;
-      margin: 0 auto;
-      background: #fff;
-      box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
-      overflow: hidden;
-
-      .tabs {
-        span {
-          display: block;
-          width: 50%;
-          height: 50px;
-          box-sizing: border-box;
-          border-top: 2px #eee solid;
-          background: #eee;
-          line-height: 48px;
-          text-align: center;
-          cursor: pointer;
-          color: #666;
-
-          &.active {
-            color: orange;
-            border-top-color: orange;
-            background: #fff;
-            font-weight: bold;
-          }
-        }
-      }
-    }
-  }
-}
 </style>
